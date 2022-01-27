0a75997b74e8e6f50b0c93bc12a8acb2e8df8f33
Alex Martinez <j.alexandermartinez@Alexs-MacBook-Air.local>
2022-01-27 10:58:33 -0500

    imported Restaurants.json

---
+++ index.js
@@ -0,0 +1,10 @@
+// import our restaurants
+const restaurants = require('./restaurants.json');
+
+console.log(restaurants[0].name)
+
+// connect to Firstore
+
+// create a collection called "Restaurants"
+
+// add each Restaurant
